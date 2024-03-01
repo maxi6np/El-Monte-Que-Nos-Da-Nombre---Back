69da4d1dac8820d5754d2e561289b20938aecc2e
maxi6np <nopemaxi@gmail.com>
2024-03-01 13:32:05 +0100

    trabajos seeder

--- app/Http/Controllers/PuntosInteresController.php
+++ app/Http/Controllers/PuntosInteresController.php
@@ -12,23 +12,14 @@ class PuntosInteresController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function mostrarMapa(Request $request)
-    {
-        $apiKey = '4531f429413fa5dfbd3768ec3040c187';
-        $url_servicio = "https://api.openweathermap.org/data/2.5/weather?lat={$request->latitud}&lon={$request->longitud}&appid=$apiKey`";
-        $curl = curl_init($url_servicio);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $respuesta_curl = curl_exec($curl);
-        curl_close($curl);
-        $datosTiempo = json_decode($respuesta_curl, true);
 
-        // igual al data de un fetch
-        return response()->json($datosTiempo);
-    }
-
+    
     public function getPuntos(){
         $puntosInteres = PuntoInteres::all();
         return response()->json($puntosInteres);
+    }
+
+    public function getPuntosConTrabajos(){
+        
     }
 }
--- app/Models/Trabajo.php
+++ app/Models/Trabajo.php
@@ -9,6 +9,16 @@ class Trabajo extends Model
 {
     use HasFactory;
     protected $table = 'trabajos';
+    protected $fillable = [
+        'nombre',
+        'texto',
+        'URL',
+        'tipo',
+        'idioma',
+        'movil',
+        'id_punto_interes'
+    ];
+    public $timestamps = false;
 
     public function punto(){
         return $this->BelongsTo(PuntoInteres::class);
